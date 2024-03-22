6aa4c783c7c847c0fe2c832c28e7dc3190992b75
nishantak09 <nishantak09@gmail.com>
2024-03-23 03:44:56 +0530

    something

--- website/nlp.py
+++ website/nlp.py
@@ -28,7 +28,7 @@ def modify_des(context, description, type):
         A research conducted concluded 26 techniques for prompt engineering to boost response efficacy by at least 60%. 
         Those techniques have all been hereby implemented in our prompt engineering.
     """
-    prompt = f"I am applying for a job of {context}. The audience is a recrutier expert in the field of {context}. Your task is to modify my given description to create a clear, brief and goal-oriented text paragraph for the specified purpose which is phrased clearly using ATS-friendly and recruiter-friendly language. Use natural human-like language. Do not use AI-sounding words. Do not use these words- 'beacon', 'leveraging', 'robust'. Include {context} related jargon and clad it with industry-specific keywords that recruiters and ATS look for, such as but not limited to {keywords} wherever applicable. I am going to tip $9999 for a better solution. Don't use any formatting."
+    prompt = f"I am applying for a job of {context}. The audience is a recrutier expert in the field of {context}. Your task is to modify my given description to create a clear, brief and goal-oriented text paragraph for the specified purpose which is phrased clearly using ATS-friendly and recruiter-friendly language. Use natural human-like language. Do not use AI-sounding words. Do not use these words- 'beacon', 'leveraging', 'robust'. Include {context} related jargon and clad it with industry-specific keywords that recruiters and ATS look for, such as but not limited to {keywords} only wherever applicable. I am going to tip $9999 for a better solution. Don't use any formatting."
 
     # Leadership Experience modification
     if type == "lex":
@@ -48,6 +48,7 @@ def modify_des(context, description, type):
 
     prompt = prompt + description + "\nAlso spell-check it and fix any grammatical errors. I will charge a penalty if my instructions are not followed"
     return model.generate_content(prompt).text
+ 
 
 if __name__ == '__main__':
     print(modify_des("say hi"))
